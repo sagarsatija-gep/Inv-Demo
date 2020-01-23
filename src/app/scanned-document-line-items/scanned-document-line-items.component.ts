263e83ed93fcb00218537429ca9c31aeda9070e5
Govind Chauhan <Govind.Chauhan@gep.com>
2020-01-23 17:44:58 +0530

    +++

--- src/app/scanned-document-line-items/scanned-document-line-items.component.ts
+++ src/app/scanned-document-line-items/scanned-document-line-items.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface LineItem {
+  id?: number;
+  itemNumber: string;
+  description: string;
+  uom: string;
+  poQuantity: number;
+  receivedQuantity: number;
+}
+
+const LINEITEMS: LineItem[] = [
+  {
+    itemNumber: "2063777",
+    description: "138-7576: BRACKET ASSEMBLY",
+    uom: "Each",
+    poQuantity: 77,
+    receivedQuantity: 20
+  },
+  {
+    itemNumber: "2030539",
+    description: "Morse Cutting Tools 82062",
+    uom: "Each",
+    poQuantity: 200,
+    receivedQuantity: 50
+  },
+  {
+    itemNumber: "5000206",
+    description: "Spocket & Gear",
+    uom: "Each",
+    poQuantity: 20,
+    receivedQuantity: 20
+  },
+  {
+    itemNumber: "2030473",
+    description: "Gear Air Moter",
+    uom: "Each",
+    poQuantity: 20,
+    receivedQuantity: 20
+  }
+];
 
 @Component({
   selector: 'app-scanned-document-line-items',
   templateUrl: './scanned-document-line-items.component.html',
   styleUrls: ['./scanned-document-line-items.component.css']
 })
-export class ScannedDocumentLineItemsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ScannedDocumentLineItemsComponent {
+  get lineItems(): LineItem[] {
+    return LINEITEMS
+      .map((lineItem, i) => ({ id: i + 1, ...lineItem }));
   }
-
+  constructor() { }
 }
