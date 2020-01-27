36ea377fa81007ea7193d8ccd334ab1180254a58
Kishan Mund <Kishan.Mund@gep.com>
2020-01-27 13:36:26 +0530

    +

---
+++ src/app/asn-gr/asn-gr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asn-gr',
+  templateUrl: './asn-gr.component.html',
+  styleUrls: ['./asn-gr.component.css']
+})
+export class AsnGrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
